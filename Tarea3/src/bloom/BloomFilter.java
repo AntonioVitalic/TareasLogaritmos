a21660245599876e01c9c45ea1212152f0f3e2eb
JuanCidL <jlcl.juancid@gmail.com>
2024-07-10 17:14:21 -0400

    Guava added

---
+++ Tarea3/src/bloom/BloomFilter.java
@@ -0,0 +1,14 @@
+package bloom;
+
+import java.nio.charset.Charset;
+
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
+
+public class BloomFilter {
+    public static void main(String[] args) {
+        // HashFunction hf = Hashing.murmur3_128();
+        // System.out.println(hf.hashString("hello", Charset.defaultCharset()).asLong());
+        System.out.println("aaaaaaaaaaaaaaa");
+    }
+}
